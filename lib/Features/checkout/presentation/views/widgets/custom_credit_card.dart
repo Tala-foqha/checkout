60b27d235e3b2a8d4e0c9bdf8a7d9aac817cf1de
tala <foqhat835@gmail.com>
2025-09-22 16:54:44 +0300

    create custom credit card

---
+++ lib/Features/checkout/presentation/views/widgets/custom_credit_card.dart
@@ -0,0 +1,42 @@
+// Features/checkout/presentation/views/widgets/custom_credit_card.dart
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+class CustomCreditCard extends StatefulWidget {
+   CustomCreditCard({super.key});
+
+  @override
+  State<CustomCreditCard> createState() => _CustomCreditCardState();
+}
+
+class _CustomCreditCardState extends State<CustomCreditCard> {
+String cardNumber='',expiryDate='',cvvCode='',cardHolderName='';
+
+bool showBackView=false;
+final GlobalKey<FormState>formKey=GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CreditCardWidget(cardNumber: cardNumber, 
+        expiryDate: expiryDate, 
+        cardHolderName: cardHolderName,
+         cvvCode: cvvCode, 
+         showBackView: showBackView, 
+         isHolderNameVisible: true,
+         onCreditCardWidgetChange: (value){}),
+         CreditCardForm(cardNumber: cardNumber, expiryDate: expiryDate, cardHolderName: cardHolderName, cvvCode: cvvCode,
+          onCreditCardModelChange: (carditCardModel){
+          cardHolderName=carditCardModel.cardHolderName;
+          cardNumber=carditCardModel.cardNumber;
+          cvvCode=carditCardModel.cvvCode;
+          expiryDate=carditCardModel.expiryDate;
+          showBackView=carditCardModel.isCvvFocused;
+          setState(() {
+            
+          });
+         }, formKey: formKey)
+      ],
+    );
+  }
+}
--- lib/Features/checkout/presentation/views/widgets/payment_details_view_body.dart
+++ lib/Features/checkout/presentation/views/widgets/payment_details_view_body.dart
@@ -1,4 +1,5 @@
 // Features/checkout/presentation/views/widgets/payment_details_view_body.dart
+import 'package:checkout_app/Features/checkout/presentation/views/widgets/custom_credit_card.dart';
 import 'package:checkout_app/Features/checkout/presentation/views/widgets/payment_method_item.dart';
 import 'package:checkout_app/Features/checkout/presentation/views/widgets/payment_methods_list_view.dart';
 import 'package:checkout_app/core/widgets/custom_appbar.dart';
@@ -9,16 +10,19 @@ class PaymentDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomAppbar(title: 'Payment Details',),
-        SizedBox(height: 32,),
-        PaymentMethods()
-        
-
-
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppbar(title: 'Payment Details',),
+          SizedBox(height: 32,),
+          PaymentMethods(),
+          CustomCreditCard()
+          
+      
+      
+        ],
+      ),
     );
   }
 }
