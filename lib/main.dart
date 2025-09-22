79a8afeaeb8ff9d6db864522a8a88f4010731d8f
tala <foqhat835@gmail.com>
2025-09-22 11:16:08 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+// main.dart
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CheckoutApp());
+}
+class CheckoutApp extends StatelessWidget {
+  const CheckoutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
