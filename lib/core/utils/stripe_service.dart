da03b11c9d157484ca57a8a3c085af75046ef054
tala <foqhat835@gmail.com>
2025-09-23 15:08:48 +0300

    create api services

---
+++ lib/core/utils/stripe_service.dart
@@ -0,0 +1,10 @@
+// core/utils/stripe_service.dart
+import 'package:checkout_app/Features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:checkout_app/Features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
+
+class StripeService {
+
+  // PaymentIntentModel createPaymentIntent(PaymentIntentInputModel paymentIntentInputmMdel){
+
+  // }
+}
